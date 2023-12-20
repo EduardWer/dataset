133fb2ca3f561a4d20888a9932f83b52cac1e1bf
Eduard_wer <eduard.unanov66@mail.ru>
2023-12-20 13:24:27 +0300

    аше

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
+
 import pandas as pd
 import numpy as np
 
 
+
+
 data = pd.read_csv("Mall_Customers.csv") # конект к файлу
 
-#print(data.head()) # выводит первые n строчки
+print(data.head()) # выводит первые n строчки
 #
 #
-# print(data.tail()) # выводит последнии n строчки
+#print(data.tail()) # выводит последнии n строчки
 #
-# print(data.info()) # выводит информацию про сsv
+#print(data.info()) # выводит информацию про сsv
 #print(data.columns) # выводит названия коллон в csv
 
 
-data.to_numpy() # конверт в numpy объект
+#data.to_numpy() # конверт в numpy объект
 
 #print(data.describe()) #возвращает основные статистические показатели для каждого числового столбца
 
@@ -35,9 +38,9 @@ data.to_numpy() # конверт в numpy объект
 #print(data[data["CustomerID"] > 20]) # вывод CustomerId  где значение больше 20
 
 
-# print(data.shape) # выводит кортеж размерность масива
+#print(data.shape) # выводит кортеж размерность масива
 
-# print(data.drop(columns=["CustomerID"])) # Удаляет коллону CustomerID
+#print(data.drop(columns=["CustomerID"])) # Удаляет коллону CustomerID
 
 #print(data.replace({"CustomerID":1},"xz")) # меняет значение customerID где  =1 на xz
 
@@ -48,10 +51,10 @@ data.to_numpy() # конверт в numpy объект
 #print(data.loc[data['Spending Score (1-100)'] == 39])
 
 
-# print(data.loc[0,"Spending Score (1-100)"]) # выбор опред ячейки данных
+#print(data.loc[0,"Spending Score (1-100)"]) # выбор опред ячейки данных
 #
-# data.loc[0,"Genre"] = 'Male'
+#data.loc[0,"Genre"] = 'Male'
 
 #print(data.values.tolist()) # преобразование в LIst
 
-data.to_csv("data.csv",index=False) # Сохранение в лист
+#data.to_csv("data.csv",index=False) # Сохранение в лист
